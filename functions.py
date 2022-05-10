c322142d76e713744f53886253326cf336636dc5
Rob <ricowilliams@protonmail.com>
2022-05-09 18:58:50 -0600

    functions practice finished

--- functions.py
+++ functions.py
@@ -4,3 +4,17 @@ def mean(mylist):
 
 
 print(mean([1, 4, 6]))
+
+
+def area(square):
+    return square**2
+
+
+print(area(3))
+
+
+def mil_ounc(oz):
+    return oz * 29.57353
+
+
+print(mil_ounc(1))
