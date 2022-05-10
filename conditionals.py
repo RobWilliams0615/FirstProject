1c03835f9dcd771fb1d54e698290bc116374b792
Rob <ricowilliams@protonmail.com>
2022-05-09 19:23:36 -0600

    conditional refresher

---
+++ conditionals.py
@@ -0,0 +1,30 @@
+def mean(value):
+    if isinstance(value, dict):
+        the_mean = sum(value.values()) / len(value)
+    else:
+        the_mean = sum(value) / len(value)
+    return the_mean
+
+
+class1_grades = {"Tom": 90.5, "Dan": 80.7, "Lisa": 75.8}
+print(mean(class1_grades))
+
+
+def temp(value):
+    if value > 7:
+        return("Warm")
+    else:
+        return("Cold")
+
+
+print(temp(7))
+
+
+def foo(password):
+    if len(password) >= 8:
+        return True
+    else:
+        return False
+
+
+print(foo('hellofishy'))
